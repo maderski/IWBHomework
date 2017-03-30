b9dfb47214106877eeca526f7d513a2e4b7bf1bd
Jason Maderski <maderski@gmail.com>
2017-03-30 19:11:29 -0400

    Removed titlebar and action bar

--- IWBInterviewHW/app/src/main/java/maderski/iwbinterviewhw/MainActivity.java
+++ IWBInterviewHW/app/src/main/java/maderski/iwbinterviewhw/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import java.util.List;
@@ -15,6 +17,9 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewAdapt
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
