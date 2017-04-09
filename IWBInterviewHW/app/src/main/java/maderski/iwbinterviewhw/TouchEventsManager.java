bfd22eda33da70039226656c3fece34ea95cd9f9
Jason Maderski <maderski@gmail.com>
2017-04-08 20:55:10 -0400

    Gets finger position

--- IWBInterviewHW/app/src/main/java/maderski/iwbinterviewhw/TouchEventsManager.java
+++ IWBInterviewHW/app/src/main/java/maderski/iwbinterviewhw/TouchEventsManager.java
@@ -2,13 +2,16 @@ package maderski.iwbinterviewhw;
 
 import android.util.Log;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import maderski.iwbinterviewhw.Helpers.CaptureTouchEventsHelper;
 import maderski.iwbinterviewhw.Helpers.TouchEventsHelper;
 import maderski.iwbinterviewhw.Helpers.ViewRectHelper;
 import maderski.iwbinterviewhw.Models.TouchEventModel;
+import maderski.iwbinterviewhw.Models.ViewRectModel;
 
 /**
  * Created by Jason on 4/8/17.
@@ -19,7 +22,7 @@ public class TouchEventsManager implements CaptureTouchEventsHelper.OnTouchListe
 
     public interface PositionCallbacks{
         void choosenPosition(int position);
-        void pressedPositions(int[] positions);
+        void pressedPositions(List<Integer> positions);
     }
 
     private TouchEventsHelper mTouchEventsHelper;
@@ -40,8 +43,71 @@ public class TouchEventsManager implements CaptureTouchEventsHelper.OnTouchListe
         for (Object object : touchEvents.entrySet()) {
             Map.Entry pair = (Map.Entry) object;
             int positionId = (int) pair.getKey();
-            TouchEventModel touchEventModel = (TouchEventModel) pair.getValue();
-            Log.d(TAG, "POSITION ID: " + String.valueOf(positionId) + " area: " + String.valueOf(touchEventModel.getArea()));
+            TouchEventModel touchEvent = (TouchEventModel) pair.getValue();
+            //Log.d(TAG, "POSITION ID: " + String.valueOf(positionId) + " area: " + String.valueOf(touchEventModel.getArea()));
+            List<Integer> touchingRectangles = getTouchedRectangle(touchEvent);
+            for (Integer position : touchingRectangles){
+                Log.d(TAG, "TOUCHING POSITION: " + String.valueOf(position));
+            }
         }
+    }
+
+    private List<Integer> getTouchedRectangle(TouchEventModel touchEvent) {
+        List<Integer> positions = new ArrayList<>();
+        HashMap<Integer, ViewRectModel> Rectangles = mRectangleHelper.getViewRectangles();
+        for(Object object : Rectangles.entrySet()){
+            Map.Entry pair = (Map.Entry) object;
+            ViewRectModel rectangle = (ViewRectModel) pair.getValue();
+            boolean doesTouchRectangle = doesTouchRectangle(touchEvent, rectangle, (int) pair.getKey());
+            if(doesTouchRectangle){
+                int position = (int) pair.getKey();
+                positions.add(position);
+            }
+        }
+        return positions;
+    }
+
+    private boolean doesTouchRectangle(TouchEventModel touchEvent, ViewRectModel rectangle, int position){
+        float x = touchEvent.getX();
+        float y = touchEvent.getY();
+        float semiMajorAxis = touchEvent.getMajorAxis()/2;
+        float semiMinorAxis = touchEvent.getMinorAxis()/2;
+        float ellipseVertexTop = y + semiMajorAxis;
+        float ellipseVertexBottom = y - semiMajorAxis;
+        float ellipseCovertexRight = x + semiMinorAxis;
+        float ellipseCovertexLeft = x - semiMinorAxis;
+        Log.d(TAG, "RECT TOP: " + String.valueOf(rectangle.getTop()) + " VERTEX TOP: " + String.valueOf(ellipseVertexTop));
+        Log.d(TAG, "RECT BTM: " + String.valueOf(rectangle.getBottom()) + " VERTEX BTM: " + String.valueOf(ellipseVertexBottom));
+        Log.d(TAG, "RECT LFT: " + String.valueOf(rectangle.getLeft()) + " VERTEX LFT: " + String.valueOf(ellipseCovertexLeft));
+        Log.d(TAG, "RECT RGT: " + String.valueOf(rectangle.getRight()) + " VERTEX RGT: " + String.valueOf(ellipseCovertexRight));
+        boolean isTouching = false;
+
+        // Checks the top part of the ellipse
+        if(ellipseVertexTop > rectangle.getTop() && ellipseVertexTop < rectangle.getBottom()){
+            if(ellipseCovertexLeft > rectangle.getLeft() && ellipseCovertexRight < rectangle.getRight()){
+                isTouching = true;
+            }
+        }
+        // Checks the bottom part of the ellipse
+        if(ellipseVertexBottom > rectangle.getTop() && ellipseVertexBottom < rectangle.getBottom()){
+            if(ellipseCovertexLeft > rectangle.getLeft() && ellipseCovertexRight < rectangle.getRight()){
+                isTouching = true;
+            }
+        }
+        // Checks the left part of the ellipse
+        if(ellipseCovertexLeft > rectangle.getLeft() && ellipseCovertexLeft < rectangle.getRight()){
+            if(ellipseVertexTop > rectangle.getTop() && ellipseVertexTop < rectangle.getBottom()){
+                isTouching = true;
+            }
+        }
+        // Checks the right part of the ellipse
+        if(ellipseCovertexRight > rectangle.getLeft() && ellipseCovertexRight < rectangle.getRight()){
+            if(ellipseVertexTop > rectangle.getTop() && ellipseVertexTop < rectangle.getBottom()){
+                isTouching = true;
+            }
+        }
+
+        Log.d(TAG, "IS TOUCHING: " + String.valueOf(isTouching) + " Position: " + String.valueOf(position));
+        return isTouching;
     }
 }
